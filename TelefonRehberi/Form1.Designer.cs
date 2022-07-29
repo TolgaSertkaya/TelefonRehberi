f2fa6adc82947eedd4a94f22001a74a3f49f88eb
lenovo <lenovo@LAPTOP-8GNSB8G4>
2022-07-29 11:34:22 +0300

    Add project files.

---
+++ TelefonRehberi/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace TelefonRehberi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rdbCalisan = new System.Windows.Forms.RadioButton();
+            this.rdbBayi = new System.Windows.Forms.RadioButton();
+            this.rdbMüsteri = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDuzenle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnKaydet);
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.rdbMüsteri);
+            this.groupBox1.Controls.Add(this.rdbBayi);
+            this.groupBox1.Controls.Add(this.rdbCalisan);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtAd);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(195, 397);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kayıt Ekle ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad:";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(35, 30);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(128, 20);
+            this.txtAd.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Rolü:";
+            // 
+            // rdbCalisan
+            // 
+            this.rdbCalisan.AutoSize = true;
+            this.rdbCalisan.Location = new System.Drawing.Point(10, 94);
+            this.rdbCalisan.Name = "rdbCalisan";
+            this.rdbCalisan.Size = new System.Drawing.Size(59, 17);
+            this.rdbCalisan.TabIndex = 3;
+            this.rdbCalisan.TabStop = true;
+            this.rdbCalisan.Text = "Çalışan";
+            this.rdbCalisan.UseVisualStyleBackColor = true;
+            // 
+            // rdbBayi
+            // 
+            this.rdbBayi.AutoSize = true;
+            this.rdbBayi.Location = new System.Drawing.Point(10, 131);
+            this.rdbBayi.Name = "rdbBayi";
+            this.rdbBayi.Size = new System.Drawing.Size(45, 17);
+            this.rdbBayi.TabIndex = 4;
+            this.rdbBayi.TabStop = true;
+            this.rdbBayi.Text = "Bayi";
+            this.rdbBayi.UseVisualStyleBackColor = true;
+            // 
+            // rdbMüsteri
+            // 
+            this.rdbMüsteri.AutoSize = true;
+            this.rdbMüsteri.Location = new System.Drawing.Point(9, 168);
+            this.rdbMüsteri.Name = "rdbMüsteri";
+            this.rdbMüsteri.Size = new System.Drawing.Size(59, 17);
+            this.rdbMüsteri.TabIndex = 5;
+            this.rdbMüsteri.TabStop = true;
+            this.rdbMüsteri.Text = "Müşteri";
+            this.rdbMüsteri.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Dahili, cep, ev, iş?";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(10, 244);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(156, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Telefon";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(10, 310);
+            this.maskedTextBox1.Mask = "(999) 000-0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(154, 20);
+            this.maskedTextBox1.TabIndex = 8;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(10, 358);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(157, 33);
+            this.btnKaydet.TabIndex = 9;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(233, 56);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(457, 353);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnDuzenle
+            // 
+            this.btnDuzenle.Location = new System.Drawing.Point(502, 422);
+            this.btnDuzenle.Name = "btnDuzenle";
+            this.btnDuzenle.Size = new System.Drawing.Size(107, 25);
+            this.btnDuzenle.TabIndex = 2;
+            this.btnDuzenle.Text = "Düzenle";
+            this.btnDuzenle.UseVisualStyleBackColor = true;
+            this.btnDuzenle.Click += new System.EventHandler(this.btnDuzenle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(615, 422);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 25);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(527, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ara:";
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(559, 12);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(131, 20);
+            this.txtAra.TabIndex = 5;
+            this.txtAra.TextChanged += new System.EventHandler(this.txtAra_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 462);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnDuzenle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Telefon Rehberi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rdbMüsteri;
+        private System.Windows.Forms.RadioButton rdbBayi;
+        private System.Windows.Forms.RadioButton rdbCalisan;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDuzenle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAra;
+    }
+}
+
---
+++ TelefonRehberi/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelefonRehberi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(Enum.GetNames(typeof(TelefonTipi)));
+        }
+
+        BindingList<Kisi> kisiListesi = new BindingList<Kisi>();
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtAd.Text.Trim()) || string.IsNullOrEmpty(maskedTextBox1.Text.Trim()) || comboBox1.SelectedIndex == -1 || (rdbBayi.Checked == false && rdbCalisan.Checked == false && rdbMüsteri.Checked == false))
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurunuz..");
+            }
+            else
+            {
+                if (duzenlenen == null)
+                {
+                    Kisi k = new Kisi();
+                    k.Adi = txtAd.Text;
+                    k.Rol = rdbBayi.Checked ? "Bayi" : rdbCalisan.Checked ? "Çalışan" : "Müşteri";
+                    k.Telefon = maskedTextBox1.Text;
+                    k.TelefonTipi = (TelefonTipi)Enum.Parse(typeof(TelefonTipi), comboBox1.SelectedItem.ToString());
+
+                    try
+                    {
+                        kisiListesi.Add(k);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                else
+                {
+                    duzenlenen.Adi = txtAd.Text;
+                    duzenlenen.Telefon = maskedTextBox1.Text;
+                    duzenlenen.Rol = rdbBayi.Checked ? "Bayi" : rdbCalisan.Checked ? "Çalışan" : "Müşteri";
+                    duzenlenen.TelefonTipi = (TelefonTipi)Enum.Parse(typeof(TelefonTipi), comboBox1.SelectedItem.ToString());
+
+                    kisiListesi.ResetBindings();
+
+                    duzenlenen = null;
+                }
+                dataGridView1.DataSource = kisiListesi;
+
+                txtAd.Text = maskedTextBox1.Text = "";
+                comboBox1.SelectedIndex = -1;
+                rdbBayi.Checked = rdbCalisan.Checked = rdbMüsteri.Checked = false;
+            }
+        }
+
+        private void txtAra_TextChanged(object sender, EventArgs e)
+        {
+
+            if (txtAra.Text == "")
+            {
+                dataGridView1.DataSource = kisiListesi;
+            }
+            else
+            {
+                BindingList<Kisi> aramaSonucu = new BindingList<Kisi>();
+
+
+                foreach (var item in kisiListesi)
+                {
+                    if (item.Adi.ToLower().Contains(txtAra.Text.Trim().ToLower()) || item.Rol.ToLower().Contains(txtAra.Text.Trim().ToLower()) || item.Telefon.ToLower().Contains(txtAra.Text.Trim().ToLower()))
+                        aramaSonucu.Add(item);
+                }
+                dataGridView1.DataSource = aramaSonucu;
+            }
+        }
+
+        Kisi duzenlenen;
+        private void btnDuzenle_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+                return;
+
+            duzenlenen = (Kisi)dataGridView1.SelectedRows[0].DataBoundItem;
+
+            txtAd.Text = duzenlenen.Adi;
+            maskedTextBox1.Text = duzenlenen.Telefon;
+            rdbBayi.Checked = duzenlenen.Rol == "Bayi" ? true : false;
+            rdbCalisan.Checked = duzenlenen.Rol == "Çalışan" ? true : false;
+            rdbMüsteri.Checked = duzenlenen.Rol == "Müsteri" ? true : false;
+
+            comboBox1.SelectedIndex = (int)duzenlenen.TelefonTipi;
+
+            //multıselect ı false yaparsak control tusu ıle bırden fazla secım yapamıyoruz bır tane secım yapabılıyoruz
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+                return;   //herhangı bırı secılı degılse dön
+
+            Kisi silinen = (Kisi)dataGridView1.SelectedRows[0].DataBoundItem;
+
+            kisiListesi.Remove(silinen);
+        }
+    }
+}
